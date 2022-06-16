f91768685b033296367a121d7d3189e7e14e38cd
JustasSab <sabaliunasjustas@gmail.com>
2022-06-16 10:50:12 +0300

    Some fixes

--- layouts/header.php
+++ layouts/header.php
@@ -1,7 +1,7 @@
 <div class="header-content">
     <h1 class="header-title">Beauty Studio</h1>
     <p class="header-description">Discover Your Style & Beauty</p>
-    <a href="about.php">
+    <a href="#">
         <button class="button-book-now" id="button-book-now" role="button">Book Now</button>
     </a>
 </div>
